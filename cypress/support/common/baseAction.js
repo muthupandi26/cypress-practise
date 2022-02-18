c71669a88003f950de0573d683e3b7c1a22980b7
Muthupandij26 <muthupandij26@gmail.com>
2022-02-18 19:22:20 +0530

    same name for integration inside folder and .feature

--- cypress/support/common/baseAction.js
+++ cypress/support/common/baseAction.js
@@ -2,4 +2,7 @@ export class BaseAction {
     openBrowser(url) {
         cy.visit(url);
     }
+    openBrowser1() {
+        cy.visit('https://www.instagram.com/?hl=en');
+    }
 }
--- cypress/support/steps/JustCheck.js
+++ cypress/support/steps/JustCheck.js
@@ -7,6 +7,7 @@ import { Constant } from "../common/constants";
 const baseAction = new BaseAction();
 
 When("Visit on any website", () => {
-    baseAction.openBrowser(Constant.BASE_URL);
+    // baseAction.openBrowser(Constant.BASE_URL);
+    baseAction.openBrowser1();
     // homepage.loginWith(login.username, login.password);
 });
